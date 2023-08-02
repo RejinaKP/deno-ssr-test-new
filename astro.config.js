0b26b1da08bccb712efd1489b6a7fb1f83c6a41f
Rejina K P <rejina.kp@citrusinformatics.com>
2023-08-02 14:18:29 +0530

    fix: add netspective home page

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 // Eventually, replace this import with the official one - we're using a
 // patched version for now.
 // import deno from "@astrojs/deno";
 import deno from "deno-astro-adapter";
-
+import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: deno(),
 });
--- src/content/about-us/_collection.ts
+++ src/content/about-us/_collection.ts
@@ -1,8 +1,9 @@
 import * as ac from 'astro:content';
 import * as s from '../../governance/information-model/schemas';
 
-import autoRoutes from "./_routes.auto.json";
-export type AutoRouteKey = keyof typeof autoRoutes;
+const collectionIdentity = 'about-us' as const;
+type CollectionEntry = ac.CollectionEntry<typeof collectionIdentity>;
+
 
 export const aboutusSevicesCollection = ac.defineCollection({ schema: s.aboutUsSchema });
 
